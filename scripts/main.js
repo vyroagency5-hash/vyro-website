bf10ef32e7d0e2a9f99d35942d3c7e3e069444bc
vyroagency5-hash <vyroagency5@gmail.com>
2026-05-02 13:11:41 +0300

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+import { openServices, closeServices, handleServicesBg,
+         openClients, closeClients, handleClientsBg,
+         openWorks, closeWorks, handleWorksBg,
+         openTeamModal, closeTeamModal, handleTeamBg
+} from "./modal.js";
+
+window.openServices = openServices;
+window.closeServices = closeServices;
+window.handleServicesBg = handleServicesBg;
+window.openClients = openClients;
+window.closeClients = closeClients;
+window.handleClientsBg = handleClientsBg;
+window.openWorks = openWorks;
+window.closeWorks = closeWorks;
+window.handleWorksBg = handleWorksBg;
+window.openTeamModal = openTeamModal;
+window.closeTeamModal = closeTeamModal;
+window.handleTeamBg = handleTeamBg;
